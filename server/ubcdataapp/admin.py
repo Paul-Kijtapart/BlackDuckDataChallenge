d6993733d37cf7f895eed3748104ca49641ca54d
Paul-Kijtapart <scc385@gmail.com>
2017-03-18 13:20:50 -0700

    fixed admine and test loading initial for first 5 rows

--- server/ubcdataapp/admin.py
+++ server/ubcdataapp/admin.py
@@ -9,4 +9,11 @@ from ubcdataapp.models import DWS
 from ubcdataapp.models import SO
 
 
-admin.site.register(Version, License, Project, DNS, DWS, SO)
+admin.site.register(Version)
+admin.site.register(License)
+admin.site.register(DNS)
+admin.site.register(DWS)
+admin.site.register(SO)
+admin.site.register(Project)
+
+
--- server/ubcdataapp/management/commands/load_initial_data.py
+++ server/ubcdataapp/management/commands/load_initial_data.py
@@ -1,4 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
+import csv
+from os import path
+import ubcdataapp
+
+# absolute path
+ORIGINAL_DATA_PATH = path.join(ubcdataapp.__path__[0], "original_data")
+SAMPLE_DATA_PATH = path.join(ORIGINAL_DATA_PATH, "sample.csv")
 
 # models
 from ubcdataapp.models import Version
@@ -13,5 +20,8 @@ class Command(BaseCommand):
     help = 'Load Initial CSV data to PostgreSQL database'
 
     def handle(self, *args, **options):
+        with open(SAMPLE_DATA_PATH, "r") as csvfile:
+            lines = csv.reader(csvfile, delimiter=';', quotechar='"')
+            for line_count in range(5):
+                print(lines.__next__())
         self.stdout.write("Successfully Load initial data to PostgreSQL database.")
-        pass
